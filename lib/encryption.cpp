249884cd3bc98213dc1de6b9ba56511aba74595c
Lucas Kanashiro <lucas-kanashiro@hotmail.com>
2014-04-19 14:02:02 -0300

    Inserting substitution methods

--- lib/encryption.cpp
+++ lib/encryption.cpp
@@ -5,19 +5,35 @@ Encryption::Encryption(){}
 string
 Encryption::substitution(string text, string key)
 {
-	return " ";  
+  string cypherText;
+
+  cypherText = this->monoalphabetic(text, key);
+  cypherText = this->vernam(cypherText, key);
+
+  return cypherText;
 }
 
 string 
 Encryption::monoalphabetic(string text, string key)
 {
-	return " ";
+  string cypherText = text;
+
+  for(unsigned int i=0; i<text.size(); i++)
+    cypherText[i] = (char) ((int)text[i] + (int)key[0]);
+
+  return cypherText;
 }
 
 string 
 Encryption::vernam(string text, string key)
 {
-	return " ";
+  string cypherText = text;
+  unsigned int j=0;
+
+  for(unsigned int i=0; i<text.size(); i++, j=((j+1)%key.size()))
+    cypherText[i] = text[i] ^ key[j];
+
+  return cypherText;
 }
 
 string 
@@ -50,8 +66,10 @@ Encryption::cypher(string text, string key, int FLAG)
   {
     case 1:
       cypherText = this->transposition(text, key);
+      break;
     case 2:
       cypherText = this->substitution(text, key);
+      break;
   }
 
   return cypherText;
--- lib/main.cpp
+++ lib/main.cpp
@@ -12,13 +12,11 @@ int main(){
 	Key key;
 	Encryption encryption;
 
-					key.generate("key.txt",512);  
+  key.generate("key.txt",512);  
   cout << key.getKeyText() << endl;
 
-	string result;
-	result = encryption.cypher("neste exercicio estou testando a transposicao de 8 blocos" , key.getKeyText(), TRANSPOSITION);
-
-	cout << result;
+	cout << "TRANSPOSICAO: [" << encryption.cypher("teste criptografiateste criptografiateste criptografiateste criptografiateste criptografiateste criptografiateste criptografiateste criptografiateste criptografiateste criptografiateste criptografia" , key.getKeyText(), TRANSPOSITION) << "]" << endl;
+  cout << "SUBSTITUICAO : [" << encryption.cypher("teste criptografiateste criptografiateste criptografiateste criptografiateste criptografiateste criptografiateste criptografiateste criptografiateste criptografiateste criptografiateste criptografia", key.getKeyText(), SUBSTITUTION) << "]" << endl;
 
 	return 0;
 }
