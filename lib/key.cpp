4cf920ac1aaf8ae7edf528d0868d102c5b0d7322
Lucas Kanashiro <lucas-kanashiro@hotmail.com>
2014-05-01 15:32:18 -0300

    Adding key generate

--- lib/key.cpp
+++ lib/key.cpp
@@ -31,35 +31,7 @@ Key::generate(string fileName,int numberOfBits)
   for(int i=0; i<(this->numberOfBits/8); i++)
   {
     char caracter = (char)(rand() % 94 + 33);
-    int length = rand() % 87 + 22;
-
-    for(int i=0; i<length; i++)
-    {
-      if(text[i] == 32)
-      {
-        text[i] = '0';
-        continue;
-      }
-      else if(text[i] == '9')
-      {
-        text[i] = 'A';
-        continue;
-      }
-      else if(text[i] == 'Z')
-      {
-        text[i] = 'a';
-        continue;
-      }
-      else if(text[i] == 'z')
-      {
-        text[i] = 32;
-        continue;
-      }
-      
-      text[i]++;
-    }
-
-    randomKey << num; 
+    randomKey << caracter; 
   }
 
   writeKeyInFile(fileName,randomKey.str());
